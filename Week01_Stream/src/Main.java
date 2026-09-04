9ddb57d8709728d7d535ae12cc2a45c85eb1eaba
juwonkim <mint00gaga@gmail.com>
2026-09-04 09:27:33 +0900

    스트림 기초 , 생성 및 중간연산자

--- Week01_Stream/src/Main.java
+++ Week01_Stream/src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,5 +11,49 @@ public class Main {
         Stream<Integer> intStream = list.stream();
         Stream.of(new String[]{"a","b","c"}, "d");    //스트링배열 a,b,c 가 들어간, new 사용해서.
         Stream.of("a");
+
+        List<Student> students = List.of(
+                new Student(2, 250, "달봉이"),
+                new Student(1, 300, "이순신"),
+                new Student(2, 170, "김유신"),
+                new Student(1, 270, "최지우"),
+                new Student(3, 370, "정해인"),
+                new Student(1, 350, "한소희")
+        );
+        Stream<Student> stream = students.stream();
+        stream.sorted(
+                Comparator.comparing(Student::getBan)
+                        .thenComparing(Student::getTotalScore)
+                )
+                .forEach(t -> System.out.println(t));
+
+    }
+
+    static class Student {
+        private int ban;
+        private int totalScore;
+        private String name;
+
+        public Student(int ban, int totalScore, String name) {
+            this.ban = ban;
+            this.totalScore = totalScore;
+            this.name = name;
+        }
+
+        public int getBan() {
+            return ban;
+        }
+        public int getTotalScore() {
+            return totalScore;
+        }
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return String
+                .format("%d 반 %s ( 총점 : %d)",ban, name,totalScore);
+        }
     }
 }
